988a9613879d890c7a4c0c927b14bc7abb737846
Brian Mulawka <mulawkabrian@gmail.com>
2025-12-15 15:00:42 +0100

    activitée 3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,4 @@
 <?php
-
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [IndexController::class, 'index']);
