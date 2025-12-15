d369271499da6fc3089add08ddb9d64251240ced
Brian Mulawka <mulawkabrian@gmail.com>
2025-12-15 17:22:37 +0100

    activitée 6 corrigé

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+
 Route::get('/', [IndexController::class, 'index']);
-Route::get('/article/{id?}', [IndexController::class, 'show']);
+Route::get('/articles/{id}', [ArticleController::class, 'show']);
