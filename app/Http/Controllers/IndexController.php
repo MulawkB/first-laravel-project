670c1964a7bb5184906420580ec2f1a92bdcd5b2
Brian Mulawka <mulawkabrian@gmail.com>
2025-12-16 17:24:21 +0100

    activité 12

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers;
 
 class IndexController extends Controller
 {
+
     public function index()
     {
-        return view('welcome', ["name" => "Brian"]);
+    $articles = [
+        ["title" => "L’IA soigne mieux", "desc" => "L’intelligence artificielle aide les médecins à diagnostiquer plus vite."],
+        ["title" => "Villes vertes", "desc" => "Les métropoles deviennent plus écologiques et durables."],
+        ["title" => "Télétravail", "desc" => "Plus de liberté, mais aussi plus de solitude."]
+    ];
+        return view('welcome', ["name" => "Brian" , "titles" => $articles]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,9 @@
 @section('title' , 'Accueil')
 @section('content')
     <h2>Bienvenue sur le site de {{ $name }} !</h2>
-    <x-article title="L’IA soigne mieux" desc="L’intelligence artificielle aide les médecins à diagnostiquer plus vite."/>
-    <x-article title="Villes vertes" desc="Les métropoles deviennent plus écologiques et durables."/>
-    <x-article title="Télétravail" desc="Plus de liberté, mais aussi plus de solitude."/>
+    @foreach ($titles as $title)
+    <li>
+        {{ $title['title'] }} : {{ $title['desc'] }}
+    </li>
+    @endforeach
 @endsection
