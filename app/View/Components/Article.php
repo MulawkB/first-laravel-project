8e6da32c76f51d936650a0b53fb025aa23f913e3
Brian Mulawka <mulawkabrian@gmail.com>
2025-12-16 16:44:47 +0100

    activité 11

---
+++ app/View/Components/Article.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Article extends Component
+{
+    public $title;
+    public $desc;
+    public function __construct($title,$desc)
+    {
+        $this->title = $title;
+        $this->desc = $desc;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.article');
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,4 +2,7 @@
 @section('title' , 'Accueil')
 @section('content')
     <h2>Bienvenue sur le site de {{ $name }} !</h2>
+    <x-article title="L’IA soigne mieux" desc="L’intelligence artificielle aide les médecins à diagnostiquer plus vite."/>
+    <x-article title="Villes vertes" desc="Les métropoles deviennent plus écologiques et durables."/>
+    <x-article title="Télétravail" desc="Plus de liberté, mais aussi plus de solitude."/>
 @endsection
