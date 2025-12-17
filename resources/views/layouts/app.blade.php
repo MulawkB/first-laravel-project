d757456bc02586f8e4e4cf3115edead31d88e1a2
Brian Mulawka <mulawkabrian@gmail.com>
2025-12-17 15:52:50 +0100

    activité 16

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,8 @@
 <!-- resources/views/layouts/app.blade.php -->
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<link rel="stylesheet" href="{{ asset('resources/css/app.css') }}">
+<link rel="stylesheet" href="{{ asset('resources/js/app.js') }}">
 <head>
     <meta charset="UTF-8">
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
